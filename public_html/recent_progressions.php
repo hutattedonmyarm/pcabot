04a873bdb8a26aa1e1fb45fc91aec78dbabd48bf
hutattedonmyarm <hutattedonmyarm@icloud.com>
2017-11-02 21:24:27 +0100

    Added history sorting

--- public_html/recent_progressions.php
+++ public_html/recent_progressions.php
@@ -4,8 +4,54 @@
 	<meta charset="utf-8" />
 	<title>Pnut.io PCA progression history</title>
 	<link rel="stylesheet" href="styles/style_index.css" />
+	<script>
+		function sortTable(n) {
+			var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+			table = document.getElementById("progressionTable");
+			switching = true;
+			dir = "asc";
+			while (switching) {
+				switching = false;
+				rows = table.getElementsByTagName("tr");
+				for (i = 1; i < (rows.length - 1); i++) {
+					shouldSwitch = false;
+					x = rows[i].getElementsByTagName("td")[n];
+					y = rows[i + 1].getElementsByTagName("td")[n];
+					if (dir == "asc") {
+						if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+							shouldSwitch= true;
+							break;
+						}
+					} else if (dir == "desc") {
+						if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+							shouldSwitch= true;
+							break;
+						}
+					}
+				}
+				if (shouldSwitch) {
+					rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+				  	if (switchcount == 0) {
+					  	var headers = Array.from(rows[0].getElementsByTagName("th"));
+					  	headers.forEach(function(element) {
+					  		element.innerHTML = element.innerHTML.replace('▲', '');
+					  		element.innerHTML = element.innerHTML.replace('▼', '');
+					  	});
+					  	headers[n].innerHTML += dir == 'asc' ? '▲' : '▼';
+					}
+					switching = true;
+					switchcount ++; 
+				} else {
+					if (switchcount == 0 && dir == "asc") {
+				   	dir = "desc";
+				   	switching = true;
+					}
+				}
+			}
+		}
+	</script>
 </head>
-<body>
+<body onload="sortTable(3)">
 	<p>Recent PCA progressions:</p>
 	<?php
 	$history_file = 'history.json';
@@ -19,13 +65,13 @@
 		die();
 	}
 	?>
-	<table>
+	<table id="progressionTable">
 		<tbody>
 			<tr>
-				<th>User</th>
-				<th>PCA</th>
-				<th>Post ID</th>
-				<th>Date</th>
+				<th onclick="sortTable(0)">User</th>
+				<th onclick="sortTable(1)">PCA</th>
+				<th onclick="sortTable(2)">Post ID</th>
+				<th onclick="sortTable(3)">Date</th>
 			</tr>
 			<?php
 			foreach ($recent_changes as $entry) {
