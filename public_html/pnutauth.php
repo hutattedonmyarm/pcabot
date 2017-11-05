b0a029efb549ac57c078ae8a0110677de80c72bd
hutattedonmyarm <hutattedonmyarm@icloud.com>
2017-11-05 21:10:58 +0100

    Added messages to auth scope

--- public_html/pnutauth.php
+++ public_html/pnutauth.php
@@ -1,7 +1,7 @@
 <?php
 $redirect_uri = 'https://wedro.online/pnutauth.php';
 $client_id = 'RwHDh73PtU0It4DdKhwh2GEagBoO1ELD';
-$client_secret = file_get_contents('../clientsecret')
+$client_secret = file_get_contents('../clientsecret');
 if (isset($_GET['code'])) {
 	$code = $_GET['code'];
 	$postdata = http_build_query(
@@ -25,6 +25,6 @@ if (isset($_GET['code'])) {
 	file_put_contents('../access_token', $resp['access_token']);
 	header('Location: https://wedro.online/Check_PCA.php');
 } else {
-	header('Location: https://pnut.io/oauth/authenticate?client_id='.$client_id.'&redirect_uri='.urlencode($redirect_uri).'&scope=write_post&response_type=code');
+	header('Location: https://pnut.io/oauth/authenticate?client_id='.$client_id.'&redirect_uri='.urlencode($redirect_uri).'&scope=write_post,messages&response_type=code');
 }
 ?>
