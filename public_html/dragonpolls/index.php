8d19759bb7635476e171a232d249de83f8baa73a
hutattedonmyarm <hutattedonmyarm@icloud.com>
2018-04-01 21:35:58 +0200

    Changed redirect from header to JS

--- public_html/dragonpolls/index.php
+++ public_html/dragonpolls/index.php
@@ -17,6 +17,7 @@ if (isset($_SESSION['polls_auth_token'])) {
 if ($auth_token == null) { //Not yet authorized
 	$client_id = 'kAA6Qzi6ErYcqg12ljZCGie_9u3GVXwv';
 	$redirect_uri = 'https://wedro.online/dragonpolls/index.php';
+	#$redirect_uri = 'http://localhost/dragonpolls/index.php';
 	if (isset($_GET['poll'])) {
 		$redirect_uri .= '?poll='.$_GET['poll'];
 	}
@@ -42,7 +43,8 @@ if ($auth_token == null) { //Not yet authorized
 		$context = stream_context_create($context_options);
 		$resp = json_decode(file_get_contents('https://api.pnut.io/v0/oauth/access_token', false, $context), true);
 		$_SESSION['polls_auth_token'] = $resp['access_token'];
-		header($redirect_uri);
+		#header($redirect_uri);
+		echo 'Redirecting to <a href="'.$redirect_uri.'">'.$redirect_uri.'</a><script>window.location.replace("'.$redirect_uri.'");</script>';
 	} else { //Ask user to authorize
 		echo '<a href="https://pnut.io/oauth/authenticate?client_id='.$client_id.'&redirect_uri='.urlencode($redirect_uri).'&scope=polls,write_post&response_type=code">Authorize with pnut.io</a>';
 		die();
